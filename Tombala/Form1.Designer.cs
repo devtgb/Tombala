503a1d31464ef8d46c47ae9c484dec652b0383af
tugbagull0503 <gull.tugba@gmail.com>
2017-02-25 19:02:24 +0200

    First Commit

---
+++ Tombala/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Tombala
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.yeniOyunToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hakkımızdaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtKalanCekilis = new System.Windows.Forms.TextBox();
+            this.txtSonCekilen = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstCekilenler = new System.Windows.Forms.ListBox();
+            this.btnSayiCek = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yeniOyunToolStripMenuItem,
+            this.hakkımızdaToolStripMenuItem,
+            this.çıkışToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(719, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // yeniOyunToolStripMenuItem
+            // 
+            this.yeniOyunToolStripMenuItem.Name = "yeniOyunToolStripMenuItem";
+            this.yeniOyunToolStripMenuItem.Size = new System.Drawing.Size(74, 20);
+            this.yeniOyunToolStripMenuItem.Text = "Yeni Oyun";
+            this.yeniOyunToolStripMenuItem.Click += new System.EventHandler(this.yeniOyunToolStripMenuItem_Click);
+            // 
+            // hakkımızdaToolStripMenuItem
+            // 
+            this.hakkımızdaToolStripMenuItem.Name = "hakkımızdaToolStripMenuItem";
+            this.hakkımızdaToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.hakkımızdaToolStripMenuItem.Text = "Hakkımızda";
+            this.hakkımızdaToolStripMenuItem.Click += new System.EventHandler(this.hakkımızdaToolStripMenuItem_Click);
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.çıkışToolStripMenuItem.Text = "Çıkış";
+            this.çıkışToolStripMenuItem.Click += new System.EventHandler(this.çıkışToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel1.Controls.Add(this.txtKalanCekilis);
+            this.panel1.Controls.Add(this.txtSonCekilen);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lstCekilenler);
+            this.panel1.Controls.Add(this.btnSayiCek);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(5, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(651, 60);
+            this.panel1.TabIndex = 3;
+            // 
+            // txtKalanCekilis
+            // 
+            this.txtKalanCekilis.BackColor = System.Drawing.Color.Wheat;
+            this.txtKalanCekilis.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtKalanCekilis.Location = new System.Drawing.Point(407, 0);
+            this.txtKalanCekilis.Multiline = true;
+            this.txtKalanCekilis.Name = "txtKalanCekilis";
+            this.txtKalanCekilis.ReadOnly = true;
+            this.txtKalanCekilis.Size = new System.Drawing.Size(91, 57);
+            this.txtKalanCekilis.TabIndex = 14;
+            // 
+            // txtSonCekilen
+            // 
+            this.txtSonCekilen.BackColor = System.Drawing.Color.Wheat;
+            this.txtSonCekilen.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSonCekilen.Location = new System.Drawing.Point(74, 3);
+            this.txtSonCekilen.Multiline = true;
+            this.txtSonCekilen.Name = "txtSonCekilen";
+            this.txtSonCekilen.ReadOnly = true;
+            this.txtSonCekilen.Size = new System.Drawing.Size(87, 54);
+            this.txtSonCekilen.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(345, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 18);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "KALAN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(3, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 18);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "ÇEKİLEN";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 18);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "SON";
+            // 
+            // lstCekilenler
+            // 
+            this.lstCekilenler.BackColor = System.Drawing.Color.Wheat;
+            this.lstCekilenler.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lstCekilenler.FormattingEnabled = true;
+            this.lstCekilenler.Location = new System.Drawing.Point(230, 3);
+            this.lstCekilenler.Name = "lstCekilenler";
+            this.lstCekilenler.Size = new System.Drawing.Size(109, 56);
+            this.lstCekilenler.TabIndex = 8;
+            // 
+            // btnSayiCek
+            // 
+            this.btnSayiCek.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.btnSayiCek.Enabled = false;
+            this.btnSayiCek.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnSayiCek.FlatAppearance.BorderSize = 0;
+            this.btnSayiCek.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSayiCek.Location = new System.Drawing.Point(504, 4);
+            this.btnSayiCek.Name = "btnSayiCek";
+            this.btnSayiCek.Size = new System.Drawing.Size(198, 55);
+            this.btnSayiCek.TabIndex = 7;
+            this.btnSayiCek.Text = "Sayı Çek";
+            this.btnSayiCek.UseVisualStyleBackColor = false;
+            this.btnSayiCek.Click += new System.EventHandler(this.btnSayiCek_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(345, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "ÇEKİLİŞ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(158, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Çekilenler";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Location = new System.Drawing.Point(0, 102);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(656, 284);
+            this.panel2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SaddleBrown;
+            this.ClientSize = new System.Drawing.Size(719, 441);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Tombala";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem yeniOyunToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hakkımızdaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSayiCek;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstCekilenler;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSonCekilen;
+        private System.Windows.Forms.TextBox txtKalanCekilis;
+    }
+}
+
---
+++ Tombala/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tombala
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int sayi;
+        Button btn;
+        int butonSayisi = 90;
+        int rastgelesayi = 89;
+        bool sayivarmi = false;
+
+
+        private void yeniOyunToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            btnSayiCek.Enabled = true;
+            int sol = 0; //formun sol tarafından atanan değer
+            int alt = 4; // formun üst tarafından atanan değer
+            int bol; // bolme işlemindeki amaç formun boyutuna göre butonları sıralı bir şekilde görebilmek için
+            bol = Convert.ToInt32(Math.Ceiling(Math.Sqrt(butonSayisi)));
+
+            for (int i = 1; i <= butonSayisi; i++)  // girilen buton sayısına göre döngü şartı sağlanana kadar oluşturmakta
+            {
+
+                btn = new Button();
+                btn.Name = i.ToString();
+                btn.AutoSize = false;
+                btn.BackColor = Color.SteelBlue;
+                btn.Font = new Font(btn.Font.FontFamily.Name, 18);
+                btn.Size = new Size(this.Width / bol, this.Height / (bol * 2) + 20);
+                btn.Text = i.ToString();
+                btn.Location = new Point(sol, alt);
+                panel2.Controls.Add(btn);
+                sol += btn.Width;
+
+                if (sol + this.Width / bol > this.Width)
+                {
+                    sol = 1;
+                    alt += this.Height / (bol * 2) + 14;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Height = 508;
+            panel1.Width = this.Width;
+            panel2.Width = this.Width;
+            panel2.Height = this.Height;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void çıkışToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btnSayiCek_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            sayi = rnd.Next(1, 91);
+
+             sayivarmi = AyniSayiVarmi(sayi);
+
+             if (sayivarmi == false)
+             {
+                 foreach (Control buton in panel2.Controls) //panelde ki butonlar kontrol ediliyor. 
+                 {
+                     Control d = (sender as Button); 
+                     d = buton;
+
+                     if (buton.GetType() == typeof(Button))
+                     {
+                         if (buton.Text == sayi.ToString()) //butonun text'i ile sayi eşitse
+                         {
+
+                             d.BackColor = Color.LightBlue;
+                             //rastgelesayi--;
+                             butonSayisi--;
+
+                             if (txtSonCekilen.Text != " ")
+                             {
+                                 txtSonCekilen.Text = " ";
+                             }
+                             txtSonCekilen.Text = sayi.ToString();
+
+
+                             if (txtKalanCekilis.Text != " ")
+                             {
+                                 txtKalanCekilis.Text = " ";
+                             }
+                             txtKalanCekilis.Text = butonSayisi.ToString();
+                             lstCekilenler.Items.Add(sayi);               
+                         }
+                     }
+                 }
+             }
+             else
+             {
+                 sayi = rnd.Next(1, 91);
+
+                 sayivarmi = AyniSayiVarmi(sayi);
+             }
+            }
+        /// <summary>
+        /// Random dan gelen sayının listboxda olup olmadığının kontrol eden method. 
+        /// </summary>
+        /// <param name="sayi"></param>
+        /// <returns></returns>
+        bool AyniSayiVarmi(int sayi)
+        {
+            bool durum = false;
+                if (lstCekilenler.Items.Contains(sayi))
+                {
+                    durum = true;
+                }
+                else
+                {
+                     durum = false;
+                }
+            return durum;
+        }
+
+        private void hakkımızdaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tombala oyunu Tuğba Gül tarafından yazılmıştır");
+        }
+        }
+    }
+
+            
+                           
+             
+         
+
+     
+     
